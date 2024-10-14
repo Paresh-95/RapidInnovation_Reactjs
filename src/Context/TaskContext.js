5af193a870780676b9840517ae8d615ca3d98d94
Paresh-95 <Pareshbhamare95@gmail.com>
2024-10-14 22:42:04 +0530

    url tweaked

--- src/Context/TaskContext.js
+++ src/Context/TaskContext.js
@@ -11,7 +11,7 @@ const TaskProvider = ({ children }) => {
  
   const fetchTasks = async () => {
     try {
-      const url = `${process.env.REACT_APP_BACKEND_API}api/v1/getAllTasks`
+      const url = `${process.env.REACT_APP_BACKEND_API}/getAllTasks`
       const response = await axios.get(url);
       setTask(response.data.data); 
     } catch (error) {
